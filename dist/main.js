9fdb16a3bc9639589b0db8d7c32bbe638cf47c02
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2024-07-30 22:48:17 -0300

    add data types

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log("TESTE");
+/* TIPOD DE DADOS */
+//string - dados de texto
+let nameUser = "Edio";
+let lastNameUser = "Gonçalves";
+let fullNameUser = `${nameUser} ${lastNameUser}`;
+console.log(fullNameUser);
+//number - inteiros, decimais, hexadecimais, binários
+let age = 38;
+//boolean - true, false (false sempre vai ser vazio ou 0)
+let statusUser = true;
+//array - variáveis indexadas. Vetores, matrizes
+let fruits = ["Maçã", "Laranja", "Banana"];
+let fruits2 = ["Maçã", "Laranja", 50];
+let fruits3 = ["Maçã", "Laranja", "Banana"];
+let company = {
+    name: "Hcode Cursos",
+    adress: "Av XX de Piracicaba, 2043",
+    city: "São Paulo",
+    state: "SP",
+};
+//date - "yyyy-mm-dd hh:mm:ss", timestamp - quantidade de segundos a partir de 1970
+let bornDate = new Date("1986 10 20");
+//tuple - array com tipod de dados definidos
+let adress = ["Av XX de Piracicaba", 2043];
+//enum - lista de valores possíveis
+var statusEnum;
+(function (statusEnum) {
+    statusEnum[statusEnum["blocked"] = 0] = "blocked";
+    statusEnum[statusEnum["released"] = 1] = "released";
+    statusEnum[statusEnum["pending"] = 2] = "pending";
+})(statusEnum || (statusEnum = {}));
+let options = 0;
+let options2 = statusEnum.pending;
+//any - qualquer tipo de dado. NÃO RECOMENDADO
+let letTeste = "false"; // vai aceitar qualquer valor e tentar usar da forma correta
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,48 @@
-console.log("TESTE");
+/* TIPOD DE DADOS */
+//string - dados de texto
+let nameUser: string = "Edio";
+let lastNameUser: string = "Gonçalves";
+let fullNameUser = `${nameUser} ${lastNameUser}`;
+console.log(fullNameUser);
+
+//number - inteiros, decimais, hexadecimais, binários
+let age: number = 38;
+
+//boolean - true, false (false sempre vai ser vazio ou 0)
+let statusUser: boolean = true;
+
+//array - variáveis indexadas. Vetores, matrizes
+let fruits: Array<string> = ["Maçã", "Laranja", "Banana"];
+let fruits2: Array<string | number> = ["Maçã", "Laranja", 50];
+let fruits3: string[] = ["Maçã", "Laranja", "Banana"];
+//object - objeto, propriedades e funcionalidades
+type companyType = {
+  name: string;
+  adress: string;
+  city: string;
+  state?: string;
+};
+
+let company = {
+  name: "Hcode Cursos",
+  adress: "Av XX de Piracicaba, 2043",
+  city: "São Paulo",
+  state: "SP",
+};
+
+//date - "yyyy-mm-dd hh:mm:ss", timestamp - quantidade de segundos a partir de 1970
+let bornDate = new Date("1986 10 20");
+//tuple - array com tipod de dados definidos
+let adress: [string, number] = ["Av XX de Piracicaba", 2043];
+//enum - lista de valores possíveis
+enum statusEnum {
+  blocked = 0,
+  released = 1,
+  pending = 2,
+}
+
+let options: statusEnum = 0;
+let options2: statusEnum = statusEnum.pending;
+
+//any - qualquer tipo de dado. NÃO RECOMENDADO
+let letTeste = "false"; // vai aceitar qualquer valor e tentar usar da forma correta
